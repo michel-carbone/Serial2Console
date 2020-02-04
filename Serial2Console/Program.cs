02aee041c48a34e9fc83e83dabc93b228ad920b0
michel <michel@arsalis.com>
2020-02-04 18:42:41 +0100

    first commit of Serial2Console console application that print to console all data received from one serial COM port hardcoded in source code

---
+++ Serial2Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace Serial2Console
+{
+    class PortDataReceived
+    {
+        public static void Main()
+        {
+            SerialPort mySerialPort = new SerialPort("COM6");
+
+            mySerialPort.BaudRate = 115200;
+            mySerialPort.Parity = Parity.None;
+            mySerialPort.StopBits = StopBits.One;
+            mySerialPort.DataBits = 8;
+            mySerialPort.Handshake = Handshake.None;
+            mySerialPort.RtsEnable = true;
+
+            mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+            mySerialPort.Open();
+
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine();
+            Console.ReadKey();
+            mySerialPort.Close();
+        }
+
+        private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            //Console.WriteLine("Data Received:");
+            Console.Write(indata);
+        }
+    }
+}
